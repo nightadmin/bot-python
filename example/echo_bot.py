7aaa33ef0cd5bd3c02ff1cd0d0428dbe4d546787
Mark Fomin <78644136+nightadmin@users.noreply.github.com>
2021-03-26 20:33:41 +0300

    Комментирование, перевод
    Бот echo_bot переведён на русский язык, добавлены дополнительные комментарии

--- example/echo_bot.py
+++ example/echo_bot.py
@@ -1,15 +1,20 @@
 from bot.bot import Bot
 from bot.handler import MessageHandler
 
-TOKEN = "" #your token here
+TOKEN = "" #вставьте сюда ваш токен от Метабота
 
 bot = Bot(token=TOKEN)
 
 
 def message_cb(bot, event):
+    #отправляем сообщение туда, откуда его получили (event.fromChat) с тем же текстом (event.text)
     bot.send_text(chat_id=event.from_chat, text=event.text)
 
-
-bot.dispatcher.add_handler(MessageHandler(callback=message_cb))
-bot.start_polling()
-bot.idle()
+def main():
+    #хэндлер для любого сообщения
+    bot.dispatcher.add_handler(MessageHandler(callback=message_cb))
+    bot.start_polling()
+    bot.idle()
+    
+if "__name__" == __main__:
+    main()
