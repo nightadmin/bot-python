55ba043b18d288b9cbbc6e31c4174a947507ad5f
Mark Fomin <78644136+nightadmin@users.noreply.github.com>
2021-03-26 20:44:02 +0300

    Комментирование, перевод

--- example/scheduled_bot.py
+++ example/scheduled_bot.py
@@ -2,25 +2,28 @@ import schedule
 import time
 from bot.bot import Bot
 
-TOKEN = "" #your toke here
+TOKEN = "" #вставьте сюда ваш токен от Метабота
 bot = Bot(token=TOKEN)
 
 chats_to_send_notifications = [
-    "chat_to_recieve_messages",  # nick of a public group chat
-    "AoLFuNFynm67V2xGFX000",  # stamp of private group chat
-    "some_nick",  # nick of a person
-    "68250238000@chat.agent"  # default chat id
+    "na_group",  # ник чата для рассылки
+    "AoLFuNFynm67V2xGFX000",  # штамп приватной группы
+    "night_admin",  # ник человека
+    "68250238000@chat.agent"  # стандартный chatId
 ]
-text_to_send = "Let's go home!"
+text_to_send = "Эй, все идем домой! Ночь на дворе!"
 
-
+#делаем рассылку циклом
 def send_alert():
     for chat in chats_to_send_notifications:
 
         bot.send_text(chat_id=chat, text=text_to_send)
 
-
-schedule.every().day.at("19:00").do(send_alert)
+#обратите внимание: время локальное, то есть то, что установлено на сервере (в регионе сервера)
+schedule.every().day.at("21:00").do(send_alert)
 while True:
     schedule.run_pending()
     time.sleep(1)
+
+bot.idle()
+bot.startPolling()
